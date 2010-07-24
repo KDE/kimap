d6807f298223ba40241a789a668d1b9af0559f21
Alex Merry <kde@randomguy3.me.uk>
2010-07-24 14:21:27 +0000

    ++apidox
    svn path=/trunk/KDE/kdepimlibs/; revision=1154008

--- getmetadatajob.h
+++ getmetadatajob.h
@@ -30,10 +30,38 @@ class Session;
 struct Message;
 class GetMetaDataJobPrivate;
 
+/**
+ * Fetches mailbox metadata.
+ *
+ * Provides support for the IMAP METADATA extension; both the
+ * final RFC version
+ * (<a href="http://tools.ietf.org/html/rfc5464">RFC 5464</a>)
+ * and the older, incompatible draft version (known as ANNOTATEMORE)
+ * (<a
+ * href="http://tools.ietf.org/html/draft-daboo-imap-annotatemore-07"
+ * >draft-daboo-imap-annotatemore-07</a>).  See setServerCompatibility().
+ *
+ * This job can only be run when the session is in the
+ * authenticated (or selected) state.
+ *
+ * If the server supports ACLs, the user will need the
+ * Acl::Lookup right on the mailbox, as well as one of
+ * - Acl::Read
+ * - Acl::KeepSeen
+ * - Acl::Write
+ * - Acl::Insert
+ * - Acl::Post
+ * Otherwise, the user must be able to list the mailbox
+ * and either read or write the message content.
+ *
+ * Note also that on servers that implement the Annotatemore
+ * version of the extension, only Acl::Lookup rights are
+ * required (ie: the user must be able to list the mailbox).
+ */
 class KIMAP_EXPORT GetMetaDataJob : public MetaDataJobBase
 {
   Q_OBJECT
-  Q_DECLARE_PRIVATE(GetMetaDataJob)
+  Q_DECLARE_PRIVATE( GetMetaDataJob )
 
   friend class SessionPrivate;
 
@@ -41,50 +69,121 @@ class KIMAP_EXPORT GetMetaDataJob : public MetaDataJobBase
     explicit GetMetaDataJob( Session *session );
     virtual ~GetMetaDataJob();
 
+    /**
+     * Used to specify the depth of the metadata heirachy to walk.
+     */
     enum Depth {
-      NoDepth = 0,
-      OneLevel,
-      AllLevels
+      NoDepth = 0, /**< Only the requested entries */
+      OneLevel,    /**< The requested entries and all their direct children */
+      AllLevels    /**< The requested entries and all their descendants */
     };
 
-    Q_DECLARE_FLAGS(Depths, Depth)
+    Q_DECLARE_FLAGS( Depths, Depth )
 
     /**
-     * Add an entry, attribute pair to query, if the job is operating in Annotatemore mode, the attribute name is used.In Metadata mode the @param attribute is ignored.
-     * @param entry the metadata entry name
-     * @param attribute the attribute name, in Annotatemore mode
+     * Add an entry to the query list.
+     *
+     * See SetMetaDataJob for a description of metadata entry names.
+     *
+     * When operating in Annotatemore mode, you should provide an attribute
+     * name.  Typically this will be "value", "value.priv" or "value.shared",
+     * although you might want to fetch the "content-type" or
+     * "content-language" attributes as well.
+     *
+     * @param entry      the metadata entry name
+     * @param attribute  the attribute name, in Annotatemore mode
      */
-     void addEntry(const QByteArray &entry, const QByteArray &attribute = QByteArray());
+     void addEntry( const QByteArray &entry, const QByteArray &attribute = QByteArray() );
 
     /**
-     * Only entry values that are less than or equal in octet size to the specified @param size limit are returned. In
-     * Annotatemore mode, this setting is  ignored.
+     * Limits the size of returned metadata entries.
+     *
+     * In order to save time or bandwidth, it is possible to prevent the
+     * server from returning metadata entries that are larger than a
+     * certain size.  These entries will simply not appear in the
+     * list returned by allMetaData(), and will not be accessible using
+     * metaData().
+     *
+     * Note that this is only used when the server capability mode is
+     * Metadata.
+     *
+     * The default is no limit (-1).  A value of less than -1 will cause
+     * the job to fail.
+     *
+     * @param size  the entry size limit, in octets, or -1 for no limit
      */
-    void setMaximumSize(qint64 size);
+    void setMaximumSize( qint64 size );
 
     /**
-     * Default is NoDepth. In Annotatemore mode, this setting is  ignored.
-     * @param depth
+     * Sets whether to retreive children or descendants of the requested entries.
+     *
+     * Metadata entry names are heirachical, much like UNIX path names.
+     * It therefore makes sense to ask for an entry and all its children
+     * (OneLevel) or an entry and all its descendants (AllLevels).
+     *
+     * For example, /shared/foo/bar/baz is a child of /shared/foo/bar and a
+     * descendent of /shared/foo.  So if you request the entry "/shared/foo"
+     * with depth NoDepth, you will only get the "/shared/foo" entry.  If
+     * you set the depth to OneLevel, you will also get "/shared/foo/bar".
+     * If you set the depth to AllLevels, you will also get
+     * "/shared/foo/bar/baz", and every other metadata entry that starts
+     * with "/shared/foo/".
+     *
+     * Note that this is only used when the server capability mode is
+     * Metadata.
+     *
+     * @param depth  the depth of the metadata tree to return
      */
-    void setDepth(Depth depth);
+    void setDepth( Depth depth );
 
     /**
-     * Get the metadata. If used in Metadata mode, @param attribute is ignored.
-     * @param mailBox
-     * @param entry
-     * @param attribute
-     * @return
+     * Get a single metadata entry.
+     *
+     * The metadata must have been requested using addEntry(), and
+     * the job must have completed successfully, or this method
+     * will not return anything.
+     *
+     * Note that if setMaximumSize() was used to limit the size of
+     * returned metadata, this method may return an empty QByteArray
+     * even if the metadata entry was requested and exists on the
+     * server.  This will happen when the metadata entry is larger
+     * than the size limit given to setMaximumSize().
+     *
+     * @param mailBox    the mailbox the metadata is attached to, or
+     *                   an empty string for server metadata
+     * @param entry      the entry to get
+     * @param attribute  (only in Annotatemore mode) the attribute to get
+     * @return  the metadata entry value
      */
-    QByteArray metaData(const QString &mailBox, const QByteArray &entry,
-                        const QByteArray &attribute = QByteArray()) const;
+    // XXX: what's with the mailBox argument in a class that has setMailBox()?
+    //      KJobs are not intended to be run more than once
+    QByteArray metaData( const QString &mailBox, const QByteArray &entry,
+                         const QByteArray &attribute = QByteArray() ) const;
 
     /**
      * Get all the metadata for a given mailbox.
      *
-     * @param mailBox
-     * @return
+     * The returned map is from metadata entry names to attributes or values.
+     *
+     * If operating in Metadata mode, the metadata value is stored against the
+     * empty QByteArray:
+     * @code
+     * map = job.allMetaData( "INBOX" );
+     * QByteArray value = map[ "/shared/comment" ].value( QByteArray() );
+     * @endcode
+     *
+     * The equivalent in Annotatemore mode would be:
+     * @code
+     * map = job.allMetaData( "INBOX" );
+     * QByteArray value = map[ "/comment" ].value( "value.shared" );
+     * @endcode
+     *
+     * @param mailBox  a mailbox name or an empty string for server metadata
+     * @return  a map from metadata entry names to attributes or values
      */
-    QMap<QByteArray, QMap<QByteArray, QByteArray> > allMetaData(const QString &mailBox) const;
+    // XXX: what's with the mailBox argument in a class that has setMailBox()?
+    //      KJobs are not intended to be run more than once
+    QMap<QByteArray, QMap<QByteArray, QByteArray> > allMetaData( const QString &mailBox ) const;
 
   protected:
     virtual void doStart();
