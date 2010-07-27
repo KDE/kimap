4823cb6a75abae0e07146f711596dad5dbf6e813
Romain Pokrzywka <romain@kdab.net>
2010-07-27 12:39:41 +0000

    added static library support for kdepimlibs, and enable it for WinCE
    svn path=/trunk/KDE/kdepimlibs/; revision=1155483

--- kimap_export.h
+++ kimap_export.h
@@ -24,7 +24,10 @@
 #include <kdemacros.h>
 
 #ifndef KIMAP_EXPORT
-# if defined(MAKE_KIMAP_LIB)
+# if defined(KDEPIM_STATIC_LIBS)
+   /* No export/import for static libraries */
+#  define KIMAP_EXPORT
+# elif defined(MAKE_KIMAP_LIB)
    /* We are building this library */
 #  define KIMAP_EXPORT KDE_EXPORT
 # else
