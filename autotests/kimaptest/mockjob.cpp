b6e6869ec132bb933a7a94de2c98e86d97b38c0b
Volker Krause <vkrause@kde.org>
2026-01-16 17:19:33 +0100

    Port away from string-based slot invocations

--- autotests/kimaptest/mockjob.cpp
+++ autotests/kimaptest/mockjob.cpp
@@ -37,7 +37,7 @@ void MockJob::doStart()
 {
     Q_D(MockJob);
     if (isNull()) {
-        QTimer::singleShot(d->timeout, this, SLOT(done()));
+        QTimer::singleShot(d->timeout, this, &MockJob::done);
     } else {
         d->sessionInternal()->setSocketTimeout(d->timeout);
         d->tags << d->sessionInternal()->sendCommand(d->command);
