bfb21a7dfa3d30899cdc33628e0fde73e8289675
Noham Devillers <noham.devillers@enioka.com>
2026-06-12 12:00:11 +0200

    Add CONDSTORE tests for StoreJob

--- autotests/storejobtest.cpp
+++ autotests/storejobtest.cpp
@@ -22,49 +22,111 @@ private Q_SLOTS:
     void testStore_data()
     {
         QTest::addColumn<bool>("uidBased");
-        QTest::addColumn<qint64>("id");
-        QTest::addColumn<qint64>("uid");
+        QTest::addColumn<QList<qint64>>("ids");
+        QTest::addColumn<QList<qint64>>("uids");
+        QTest::addColumn<qint64>("lastModSeq");
         QTest::addColumn<QList<QByteArray>>("flags");
+        QTest::addColumn<QList<qint64>>("expectedModified");
         QTest::addColumn<QList<QByteArray>>("scenario");
 
         QList<QByteArray> scenario;
         scenario << FakeServer::preauth() << "C: A000001 STORE 3 FLAGS (\\Seen \\Foo)"
                  << "S: * 3 FETCH (FLAGS (\\Seen \\Foo) UID 1096)"
-                 << "S: A000001 OK STORE completed";
+                 << "S: A000001 OK Conditional Store completed";
 
-        QTest::newRow("not uid based") << false << qint64(3) << qint64(1096)
+        QTest::newRow("not uid based") << false << (QList<qint64>() << qint64(3)) << (QList<qint64>() << qint64(1096)) << qint64(0)
                                        << (QList<QByteArray>() << "\\Seen"
                                                                << "\\Foo")
-                                       << scenario;
+                                       << QList<qint64>() << scenario;
 
         scenario.clear();
         scenario << FakeServer::preauth() << "C: A000001 UID STORE 1096 FLAGS (\\Seen \\Foo)"
                  << "S: * 3 FETCH (FLAGS (\\Seen \\Foo) UID 1096)"
-                 << "S: A000001 OK STORE completed";
+                 << "S: A000001 OK Conditional Store completed";
 
-        QTest::newRow("uid based") << true << qint64(3) << qint64(1096)
+        QTest::newRow("uid based") << true << (QList<qint64>() << qint64(3)) << (QList<qint64>() << qint64(1096)) << qint64(0)
                                    << (QList<QByteArray>() << "\\Seen"
                                                            << "\\Foo")
-                                   << scenario;
+                                   << QList<qint64>() << scenario;
 
         scenario.clear();
         scenario << FakeServer::preauth() << "C: A000001 STORE 3 FLAGS (\\Seen \\Foo)"
                  << "S: * 3 FETCH (FLAGS (\\Seen \\Foo) UID 1096)"
-                 << "S: A000001 OK STORE completed";
+                 << "S: A000001 OK Conditional Store completed";
 
-        QTest::newRow("Ensure store don't sent \\Recent flag") << false << qint64(3) << qint64(1096)
+        QTest::newRow("Ensure store don't sent \\Recent flag") << false << (QList<qint64>() << qint64(3)) << (QList<qint64>() << qint64(1096)) << qint64(0)
                                                                << (QList<QByteArray>() << "\\Seen"
                                                                                        << "\\Foo"
                                                                                        << "\\Recent")
-                                                               << scenario;
+                                                               << QList<qint64>() << scenario;
+
+        scenario.clear();
+        scenario << FakeServer::preauth() << "C: A000001 STORE 3 (UNCHANGEDSINCE 123456789) FLAGS (\\Seen \\Foo)"
+                 << "S: * 3 FETCH (MODSEQ (123456790) FLAGS (\\Seen \\Foo) UID 1096)"
+                 << "S: A000001 OK Conditional Store completed";
+
+        QTest::newRow("CONDTSORE not uid based") << false << (QList<qint64>() << qint64(3)) << (QList<qint64>() << qint64(1096)) << qint64(123456789)
+                                                 << (QList<QByteArray>() << "\\Seen"
+                                                                         << "\\Foo")
+                                                 << QList<qint64>() << scenario;
+
+        scenario.clear();
+        scenario << FakeServer::preauth() << "C: A000001 UID STORE 1096 (UNCHANGEDSINCE 123456789) FLAGS (\\Seen \\Foo)"
+                 << "S: * 3 FETCH (MODSEQ (123456796) FLAGS (\\Seen \\Foo) UID 1096)"
+                 << "S: A000001 OK Conditional Store completed";
+
+        QTest::newRow("CONDSTORE uid based") << true << (QList<qint64>() << qint64(3)) << (QList<qint64>() << qint64(1096)) << qint64(123456789)
+                                             << (QList<QByteArray>() << "\\Seen"
+                                                                     << "\\Foo")
+                                             << QList<qint64>() << scenario;
+
+        scenario.clear();
+        scenario << FakeServer::preauth() << "C: A000001 STORE 3 (UNCHANGEDSINCE 123456789) FLAGS (\\Seen \\Foo)"
+                 << "S: * 3 FETCH (MODSEQ (123456818) FLAGS (\\Seen \\Foo) UID 1096)"
+                 << "S: A000001 OK Conditional Store completed";
+
+        QTest::newRow("CONDSTORE Ensure store don't sent \\Recent flag")
+            << false << (QList<qint64>() << qint64(3)) << (QList<qint64>() << qint64(1096)) << qint64(123456789)
+            << (QList<QByteArray>() << "\\Seen"
+                                    << "\\Foo"
+                                    << "\\Recent")
+            << QList<qint64>() << scenario;
+
+        scenario.clear();
+        scenario << FakeServer::preauth() << "C: A000001 STORE 5,7,9 (UNCHANGEDSINCE 123456789) FLAGS (\\Deleted \\Foo)"
+                 << "S: * 5 FETCH (MODSEQ (123456818))"
+                 << "S: A000001 OK [MODIFIED 7,9] Conditional STORE failed";
+
+        QTest::newRow("CONDSTORE partial fail no FETCH") << false << (QList<qint64>() << qint64(5) << qint64(7) << qint64(9))
+                                                         << (QList<qint64>() << qint64(1096) << qint64(1098) << qint64(2000)) << qint64(123456789)
+                                                         << (QList<QByteArray>() << "\\Deleted"
+                                                                                 << "\\Foo"
+                                                                                 << "\\Recent")
+                                                         << (QList<qint64>() << qint64(7) << qint64(9)) << scenario;
+
+        scenario.clear();
+        scenario << FakeServer::preauth() << "C: A000001 STORE 5,7,9 (UNCHANGEDSINCE 123456789) FLAGS (\\Deleted \\Foo)"
+                 << "S: * 7 FETCH (MODSEQ (320162342) FLAGS (\\Seen \\Deleted))"
+                 << "S: * 5 FETCH (MODSEQ (123456818))"
+                 << "S: * 9 FETCH (MODSEQ (320162349) FLAGS (\\Answered))"
+                 << "S: A000001 OK [MODIFIED 7,9] Conditional STORE failed";
+
+        QTest::newRow("CONDSTORE partial fail with FETCH") << false << (QList<qint64>() << qint64(5) << qint64(7) << qint64(9))
+                                                           << (QList<qint64>() << qint64(1096) << qint64(1098) << qint64(2000)) << qint64(123456789)
+                                                           << (QList<QByteArray>() << "\\Deleted"
+                                                                                   << "\\Foo"
+                                                                                   << "\\Recent")
+                                                           << (QList<qint64>() << qint64(7) << qint64(9)) << scenario;
     }
 
     void testStore()
     {
         QFETCH(bool, uidBased);
-        QFETCH(qint64, id);
-        QFETCH(qint64, uid);
+        QFETCH(QList<qint64>, ids);
+        QFETCH(QList<qint64>, uids);
+        QFETCH(qint64, lastModSeq);
         QFETCH(QList<QByteArray>, flags);
+        QFETCH(QList<qint64>, expectedModified);
         QFETCH(QList<QByteArray>, scenario);
 
         FakeServer fakeServer;
@@ -75,15 +137,25 @@ private Q_SLOTS:
 
         auto job = new KIMAP::StoreJob(&session);
         job->setUidBased(uidBased);
-        job->setSequenceSet(KIMAP::ImapSet(uidBased ? uid : id));
+        auto sequenceSet = KIMAP::ImapSet();
+        sequenceSet.add(uidBased ? uids : ids);
+        job->setSequenceSet(sequenceSet);
+        job->setLastModSeq(lastModSeq);
         job->setFlags(flags);
         job->setMode(KIMAP::StoreJob::SetFlags);
         bool result = job->exec();
         QVERIFY(result);
-        if (uidBased) {
-            QVERIFY(job->resultingFlags().contains(uid));
-        } else {
-            QVERIFY(job->resultingFlags().contains(id));
+        auto modifiedSet = KIMAP::ImapSet();
+        modifiedSet.add(expectedModified);
+        QVERIFY(job->unchangedMessages() == modifiedSet);
+        auto idsToVerify = uidBased ? uids : ids;
+        for (const qint64 &id : idsToVerify) {
+            if (!expectedModified.contains(id)) {
+                QVERIFY(job->resultingFlags().contains(id));
+                if (lastModSeq > 0) {
+                    QVERIFY(job->resultingModSeqs().contains(id));
+                }
+            }
         }
 
         fakeServer.quit();
